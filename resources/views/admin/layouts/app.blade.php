d75df9d329ed31e2b980f608a9ba65797e278c23
claudiomecenas <claudio@penseon.com.br>
2024-06-17 00:02:34 -0300

    feat: Add Layout

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -1,26 +1,5 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title') - CRUD</title>
-    <!-- Scripts -->
-    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
+@include('admin.layouts.header')
 
-<header class="py-8 text-center text-sm text-black dark:text-white/70">
-    Developed by <a href="https://github.com/claudiomecenas" class="underline">Cláudio Mecenas</a>
-</header>
+@yield('content')
 
-<body class="font-sans antialiased dark:bg-black dark:text-white/50 py-4 px-4 bg-gray-50">
-
-    @yield('content')
-
-</body>
-
-<footer class="py-16 text-center text-sm text-black dark:text-white/70">
-    Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
-</footer>
-</html>
+@include('admin.layouts.footer')
