c44f8b552825fbf3a1f89af150b40daa8b77c811
claudiomecenas <claudio@penseon.com.br>
2024-06-16 23:47:29 -0300

    feat: Add Layout

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -1,20 +1,10 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>View User</title>
+@extends('admin.layouts.app')
 
-    <!-- Scripts -->
-    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
-    <script src="https://cdn.tailwindcss.com"></script>
+@section('title', 'Cadstro de Usuários')
 
+@section('content')
 
-</head>
-<body class="font-sans antialiased dark:bg-black dark:text-white/50 py-4 px-4 bg-gray-50">
-
-    <h1 class="text-3xl font-bold mt-8 text-center">Cadastrar Novo Usuários</h1>
+<h1 class="text-3xl font-bold mt-8 text-center">Cadastrar Novo Usuários</h1>
 
 
     <div class="max-w-7xl mx-auto flex flex-col mt-20 mb-20">  
@@ -58,5 +48,4 @@
 
     </div>
 
-</body>
-</html>
+@endsection
