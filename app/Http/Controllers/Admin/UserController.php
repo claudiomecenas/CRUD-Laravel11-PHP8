93a9808270e284a9c9d64fddff3681d16eec1528
claudiomecenas <claudio@penseon.com.br>
2024-06-17 17:57:27 -0300

    refactor: UserController Store | request->validated

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -38,7 +38,7 @@ class UserController extends Controller
         // $user->password = $request->password;
         // $user->save();
 
-        User::create($request->validate());
+        User::create($request->validated());
         return redirect()
             ->route('users.index')
             ->with('success', 'Usuário criado com sucesso!');
